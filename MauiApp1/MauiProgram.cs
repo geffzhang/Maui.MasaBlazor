7d8df98b7c09b8aab586ade3798cc42ebfadb0d2
geffzhang <geffzhang@qq.com>
2022-09-12 08:26:32 +0800

    feat :  升级到maui 1.0 + masa blazor 0.5

--- MauiApp1/MauiProgram.cs
+++ MauiApp1/MauiProgram.cs
@@ -9,14 +9,12 @@ public static class MauiProgram
 	{
 		var builder = MauiApp.CreateBuilder();
 		builder
-			.RegisterBlazorMauiWebView()
 			.UseMauiApp<App>()
 			.ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 			});
-
-		builder.Services.AddBlazorWebView();
+        builder.Services.AddMauiBlazorWebView();
 		builder.Services.AddMasaBlazor();
 		return builder.Build();
 	}
